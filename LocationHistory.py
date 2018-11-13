d6731f40d98ac400fa34495d8fad3e3070b2b6fa
lystsin <kazuki.ouchiyama@gmail.com>
2018-11-14 01:14:38 +0900

    [update]ログの改善
    [update]ログの改善
    
    ロガーを関数の外に出して、他の関数でも使用できるように変更。
    関数の開始終了ログ追加

--- LocationHistory.py
+++ LocationHistory.py
@@ -10,15 +10,16 @@ import json
 import logging
 from datetime import datetime
 
+# loggerの設定
+logging.config.fileConfig('logging.conf')
+logger = logging.getLogger()
 
 DATA_DIR = 'data'
 READ_FILE_NAME = 'locations.json'
 
 
 def main():
-    # loggerの設定
-    logging.config.fileConfig('logging.conf')
-    logger = logging.getLogger()
+    logger.info('start main')
     
     # locationsファイルの読み込み
     locations = load_locations()
@@ -28,21 +29,32 @@ def main():
     
     # 一日ずつ処理
     for k, v in sorted(location_day.items(), key=lambda x: x[0]):
+        #月ごとのディレクトリ作成（存在しない場合のみ）
         dir_path = DATA_DIR + '/' + k[:6]
         os.makedirs(dir_path, exist_ok=True)
+        
+        #日ごとにJsonファイルに出力
         write_file_name = dir_path + '/' + k + '.json'
         with open(write_file_name, 'w') as wf:
             wf.write(json.dumps(v, ensure_ascii=False, indent=4, 
                                 sort_keys=False, separators=(',', ': ')))
     
+    logger.info('end main')
+    
 
 def load_locations():
+    logger.info('start load_locations')
+    
     with open(DATA_DIR + '/' + READ_FILE_NAME, 'r') as rf:
         dct = json.load(rf)
     return dct['locations']
 
+    logger.info('end load_locations')
+
 
 def make_location_day(locations):
+    logger.info('start make_location_day')
+    
     location_day = {}
     for location in locations:
         timestamp = timestampms_to_timestamp(location['timestampMs'])
@@ -54,6 +66,8 @@ def make_location_day(locations):
         # 年月日の辞書にタイムスタンプをキーに位置情報をセット
         location_day[timestamp[:8]][timestamp] = location
         
+    logger.info('end make_location_day')
+    
     return location_day
 
 
